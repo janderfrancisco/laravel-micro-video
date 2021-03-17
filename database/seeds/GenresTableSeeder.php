a954df070da7f60c076316dc48fa53a6e1d44589
Jander Oliveira - PC <janderfrancisco@hotmail.com>
2021-03-17 19:04:06 +0000

    first commit

---
+++ database/seeds/GenresTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+use App\Models\Genre;
+use Illuminate\Database\Seeder;
+
+class GenresTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Genre::class, 10)->create();
+    }
+}
